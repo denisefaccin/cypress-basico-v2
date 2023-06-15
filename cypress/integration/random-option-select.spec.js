07a4391a271cb5a3eb3af863b06e82d91a207de0
Denise Faccin <denisefaccin@gmail.com>
2023-06-15 12:27:49 -0300

    ajustando npm run test

--- cypress/integration/random-option-select.spec.js
+++ cypress/integration/random-option-select.spec.js
@@ -1,8 +1,8 @@
 it('selects a random option from a select dropdown', () => {
-    cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    cy.visit('src/index.html')
   
     cy.get('select option')
       .its('length', { log: false }).then(n => {
-        cy.get('select').select(Cypress._.random(n - 1))
+        cy.get('select').select(Cypress._.random(1, n - 1))
       })
   })
